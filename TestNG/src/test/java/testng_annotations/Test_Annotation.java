f416716da521993c921a36fd40418e02a2716f71
Gowri <Gowri@DESKTOP-AF33508.Dlink>
2023-09-22 16:08:48 +0530

    script to test login page using @test annotation

---
+++ TestNG/src/test/java/testng_annotations/Test_Annotation.java
@@ -0,0 +1,39 @@
+package testng_annotations;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+//script to test login page using @test annotation
+public class Test_Annotation {
+
+	@Test
+	public void testLoginpage() throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(5000);
+
+		WebElement username = driver.findElement(
+				By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+		username.sendKeys("Admin");
+		Thread.sleep(2000);
+
+		WebElement password = driver.findElement(
+				By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+		password.sendKeys("admin123");
+		Thread.sleep(2000);
+
+		WebElement login = driver
+				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+		login.click();
+		Thread.sleep(2000);
+
+		driver.quit();
+
+	}
+}
