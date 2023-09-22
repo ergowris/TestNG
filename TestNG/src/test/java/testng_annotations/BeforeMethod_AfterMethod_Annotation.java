b71a0f11383804cf6077f6212a2aa7525d053385
Gowri <Gowri@DESKTOP-AF33508.Dlink>
2023-09-22 16:15:07 +0530

    script to test valid&invalid logins using @BeforeMethod&@AfterMethod

--- TestNG/src/test/java/testng_annotations/BeforeMethod_AfterMethod_Annotation.java
+++ TestNG/src/test/java/testng_annotations/BeforeMethod_AfterMethod_Annotation.java
@@ -1,5 +1,7 @@
 package testng_annotations;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,73 +12,70 @@ import org.testng.annotations.Test;
 
 //script to test valid&invalid logins using @BeforeMethod&@AfterMethod 
 public class BeforeMethod_AfterMethod_Annotation {
+
 	static WebDriver driver;
 
 	@BeforeMethod
-	public void setUp() throws InterruptedException {
+	public void setUp() {
 
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		Thread.sleep(2000);
-
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	}
 
 	@Test
-	public void testValidLogin() throws InterruptedException {
+	public void testValidLogin() {
 
 		WebElement username = driver.findElement(
 				By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
 		username.sendKeys("Admin");
-		Thread.sleep(2000);
+
 		WebElement password = driver.findElement(
 				By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
 		password.sendKeys("admin123");
-		Thread.sleep(2000);
+
 		WebElement login = driver
 				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
 		login.click();
-		Thread.sleep(2000);
+
 		WebElement user_id = driver
 				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/p"));
 		user_id.click();
-		Thread.sleep(2000);
+
 		WebElement logout = driver
 				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]"));
 		logout.click();
-		Thread.sleep(2000);
 
 	}
 
 	@Test
-	public void testInvalidLogin() throws InterruptedException {
-		
+	public void testInvalidLogin() {
+
 		WebElement username = driver.findElement(
 				By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
 		username.sendKeys("Admin");
-		Thread.sleep(2000);
+
 		WebElement password = driver.findElement(
 				By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
 		password.sendKeys("admin13");
-		Thread.sleep(2000);
+
 		WebElement login = driver
 				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
 		login.click();
-		Thread.sleep(2000);
 
 		WebElement user_id = driver
 				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/p"));
 		user_id.click();
-		Thread.sleep(2000);
+
 		WebElement logout = driver
 				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]"));
 		logout.click();
-		Thread.sleep(2000);
 
 	}
 
 	@AfterMethod
-	public void tearDown() throws InterruptedException {
+	public void tearDown() {
 
 		driver.quit();
 	}
